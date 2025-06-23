2d332bfe1ef38926f847b57dc834f906e591f954
EvanTesta <testae92@students.rowan.edu>
2025-06-23 14:38:05 -0400

    Adding Docker files and getting environment working

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f4f9ab32138116243e80aa03e95cb9c66efbe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e07b42b7dbbdede363a89a5d3050e7677b1d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
